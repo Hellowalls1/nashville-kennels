b0e061941efdd18c46bf14eabc7817aafc2742f3
Hellowalls1 <thesingingbutcher@gmail.com>
2020-04-27 13:05:20 -0500

    completed chapter 11 and 12

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -1,9 +1,21 @@
 import React, { useState, useContext, useEffect } from "react"
 import { AnimalContext } from "../animal/AnimalProvider"
+import Animal  from "../animal/Animal"
+import { CustomerContext } from "../customer/CustomerProvider"
+import { LocationContext } from "../location/LocationProvider"
+import { Modal, ModalHeader, ModalBody } from "reactstrap"
+
 
 export const SearchResults = ({ searchTerms }) => {
     const { animals } = useContext(AnimalContext)
-    const [ filteredAnimals, setFiltered ] = useState([])
+    const { customers } = useContext(CustomerContext)
+    const { locations } = useContext(LocationContext)
+
+    const [filteredAnimals, setFiltered] = useState([])
+    const [selectedAnimal, setAnimal] = useState({animal: {id:0}, location: null, customer: null})
+
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
 
     useEffect(() => {
         if (searchTerms !== "") {
@@ -19,9 +31,27 @@ export const SearchResults = ({ searchTerms }) => {
             <h3>Results</h3>
             <div className="animals">
                 {
-                    filteredAnimals.map(animal => <div key={ animal.id }>{ animal.name }</div>)
+                    filteredAnimals.map(animal => <div
+                        className="fakeLink href"
+                        onClick={() => {
+                            const location = locations.find(l => l.id === animal.locationId)
+                            const customer = customers.find(c => c.id === animal.customerId)
+
+                            setAnimal({ animal, location, customer })
+                            toggle()
+                        }}
+                    >{animal.name}</div>)
                 }
             </div>
+
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>
+                    { selectedAnimal.animal.name }
+                </ModalHeader>
+                <ModalBody>
+                    <Animal key={selectedAnimal.animal.id} {...selectedAnimal} />
+                </ModalBody>
+            </Modal>
         </div>
     )
 }
